157e54dc42d86afbf752d0bba8f590cd756ed40d
Ian Hunter <ian@users.noreply.github.com>
2022-02-09 16:53:50 -0600

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
 
+import AnalyticsProvider from "next-lytics"
+import { GoogleAnalytics } from "next-lytics/plugins"
+
+const plugins = [
+  GoogleAnalytics({
+    trackingId: "UA-54452813-24",
+  }),
+]
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AnalyticsProvider plugins={plugins}>
+      <Component {...pageProps} />
+    </AnalyticsProvider>
+  )
 }
 
 export default MyApp
